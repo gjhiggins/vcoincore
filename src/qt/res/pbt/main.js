2c981b88f7162e3b4277e9eb0035fe271b0c12cc
Graham Higgins <gjh@bel-epa.com>
2019-06-16 15:55:17 +0100

    strip down

--- src/qt/res/pbt/main.js
+++
@@ -1,3 +0,0 @@
-$ = require('./jquery.js');  // v2.1.0-beta2                                                                                                               _ = require('underscore');
-Semantic = require('./semantic.js')
-WebTorrent = require('webtorrent.js');
