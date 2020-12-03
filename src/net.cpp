acecb89c36319d09663babd6c06787031a9e4c26
Graham Higgins <gjh@bel-epa.com>
2020-12-03 07:04:58 +0000

    add detail to oversize message report

--- src/net.cpp
+++ src/net.cpp
@@ -589,7 +589,7 @@ bool CNode::ReceiveMsgBytes(const char *pch, unsigned int nBytes, bool& complete
             return false;
 
         if (msg.in_data && msg.hdr.nMessageSize > MAX_PROTOCOL_MESSAGE_LENGTH) {
-            LogPrint(BCLog::NET, "Oversized message from peer=%i, disconnecting\n", GetId());
+            LogPrint(BCLog::NET, "Oversized message (size %u) from peer=%i, disconnecting\n", msg.hdr.nMessageSize, GetId());
             return false;
         }
 
