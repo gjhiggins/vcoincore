94f15d74a9e660603cac3d9e17c488bcf6ca9051
Graham Higgins <gjh@bel-epa.com>
2020-12-10 17:49:54 +0000

    BIP 0034 _not_ implemented in 0.9 clients

--- src/primitives/transaction.h
+++ src/primitives/transaction.h
@@ -271,7 +271,7 @@ class CTransaction
 {
 public:
     // Default transaction version.
-    static const int32_t CURRENT_VERSION=2;
+    static const int32_t CURRENT_VERSION=1; // FIXME: gjh need to upgrade to 2
 
     // Changing the default transaction version requires a two step process: first
     // adapting relay policy by bumping MAX_STANDARD_VERSION, and then later date
